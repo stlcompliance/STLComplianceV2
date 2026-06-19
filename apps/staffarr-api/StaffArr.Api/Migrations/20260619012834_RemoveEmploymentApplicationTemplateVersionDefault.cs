9ec6bb14f9bdaf16c1e33ae932aedcf1d49b9a20
tubearrteam <admin@tubearr.video>
2026-06-18 20:30:46 -0500

    Update employment application model snapshot

---
+++ apps/staffarr-api/StaffArr.Api/Migrations/20260619012834_RemoveEmploymentApplicationTemplateVersionDefault.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StaffArr.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEmploymentApplicationTemplateVersionDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Version",
+                table: "staffarr_employment_application_templates",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Version",
+                table: "staffarr_employment_application_templates",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
